84bb3f959e77d41f7c7b4399f406e60eb3559900
rachaz <rachaz@naver.com>
2024-01-05 21:02:22 +0900

    (#10)feat : 스타일 전역 설정했습니다.

---
+++ src/globalStyles.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+${reset} // 브라우저 간의 일관성을 위해 css를 초기화 
+
+*{
+    padding: 0';
+    font-family: 
+}
+
+html, body, #root{
+}
+
+li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
+
+export default GlobalStyles;
