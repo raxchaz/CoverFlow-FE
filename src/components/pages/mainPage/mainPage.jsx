edf294df00218d5949ee6f9533ac433cbf86e728
rachaz <rachaz@naver.com>
2024-01-06 15:05:51 +0900

    (#12)fix : ESlint 오류 해결했습니다.

---
+++ src/components/pages/mainPage/mainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../ui/header/header.jsx';
+import '../../../asset/sass/pages/mainPage/mainPage.scss';
+
+function MainPage() {
+  return (
+    <>
+      <Header />
+      <div className="mainpage">메인 페이지 입니다.</div>
+    </>
+  );
+}
+
+export default MainPage;
---
+++ src/components/ui/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../asset/sass/etc/header/header.scss';
+
+function Header() {
+  return (
+    <>
+      <div className="header-logo">헤더</div>
+    </>
+  );
+}
+
+export default Header;
