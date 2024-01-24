caef72707bab34c8aa93b6aa5ffd368d5d55052f
rachaz <rachaz@naver.com>
2024-01-25 08:27:25 +0900

    (#34)refactor: Route 주소 변경했습니다.

--- src/router/allRouter.js
+++ src/router/allRouter.js
@@ -67,7 +67,7 @@ const AllRouter = () => {
           }
         />
         <Route
-          path="/login/userinfo"
+          path="/login/member-info"
           element={
             <NicknamePage authenticated={authenticated} user={loggedinUser} />
           }
