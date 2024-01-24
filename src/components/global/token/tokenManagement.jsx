7dd0932e3516532cfcdd7501979a05d2bb263797
rachaz <rachaz@naver.com>
2024-01-25 00:52:30 +0900

    (#34)refactor: navigaion -> usehistory 변경했습니다.

--- src/components/global/token/tokenManagement.jsx
+++ src/components/global/token/tokenManagement.jsx
@@ -1,32 +1,32 @@
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const fetchToken = async (history) => {
-  try {
-    const response = await axios.get('https://coverflow.co.kr/api/auth/token');
-    const data = response.data;
+const fetchToken = async (navigate) => {
+try {
+const response = await axios.get('https://coverflow.co.kr/api/auth/token');
+const data = response.data;
 
-    console.log('Access Token:', data.accessToken);
-    console.log('Refresh Token:', data.refreshToken);
+console.log('Access Token:', data.accessToken);
+console.log('Refresh Token:', data.refreshToken);
 
-    // 토큰을 로컬 스토리지에 저장
-    localStorage.setItem('accessToken', data.accessToken);
-    localStorage.setItem('refreshToken', data.refreshToken);
+// 토큰을 로컬 스토리지에 저장
+localStorage.setItem('accessToken', data.accessToken);
+localStorage.setItem('refreshToken', data.refreshToken);
 
-    if (data.accessToken && data.refreshToken) {
-      history.push('/login/userinfo');
-    }
-  } catch (error) {
-    console.error('토큰을 성공적으로 받지 못했어요', error);
-  }
+if (data.accessToken && data.refreshToken) {
+  navigate('/login/userinfo');
+}
+} catch (error) {
+console.error('토큰을 성공적으로 받지 못했어요', error);
+}
 };
 
 const TokenManagement = () => {
-  const history = useHistory();
+const navigate = useNavigate();
 
-  fetchToken(history);
+fetchToken(navigate);
 
-  return null;
+return null;
 };
 
 export default TokenManagement;
