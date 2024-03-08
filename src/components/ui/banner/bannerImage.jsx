70b72f5f781cf4a2c4b0ccc261735830a3e4b1f5
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-03-08 12:05:16 +0900

    [COFLLL#138] refactor : chatIcon 스타일 수정했습니다.

--- src/components/ui/banner/bannerImage.jsx
+++ src/components/ui/banner/bannerImage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../../../asset/image/banner.svg';
 import Chat from '../../../asset/image/chaticon.svg';
+import '../../ui/banner/bannerImage.scss';
 
 const BannerImage = () => {
   return (
@@ -10,11 +11,7 @@ const BannerImage = () => {
         <span>제한 없는 정보 공유</span>
       </div>
       <img src={Banner} alt="banner" />
-      <img
-        style={{ position: 'absolute', padding: 0, right: 0 }}
-        src={Chat}
-        alt="banner"
-      />
+      <img className="img-container" src={Chat} alt="banner" />
     </div>
   );
 };
