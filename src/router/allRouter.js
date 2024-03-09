475f4ca39ca71f6efdc1296ad49651ccc9e3494f
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-03-10 08:52:07 +0900

    refactor : 필요없는 주석 삭제했습니다.

--- src/router/allRouter.js
+++ src/router/allRouter.js
@@ -34,7 +34,6 @@ import TokenManagement from '../components/global/token/tokenManagement';
 
 /* 관리자 페이지  */
 import AdminMainPage from '../components/pages/adminPage/adminMainPage.jsx';
-// import PremiumTicket from '../components/pages/premiumPage/premiumTicket.jsx';
 
 /* ===================================================================  */
 
@@ -56,10 +55,8 @@ const AllRouter = () => {
 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/login/member-info" element={<NicknamePage />} />
-
         <Route path="/store" element={<Store />} />
         <Route path="/mypage" element={<MyPage />} />
-        {/* <Route path="/premium" element={<PremiumTicket />} /> */}
         <Route path="/notification" element={<NotificationPage />} />
         <Route path="/company-regist" element={<CompanyRegistPage />} />
 
@@ -69,7 +66,6 @@ const AllRouter = () => {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/feedback" element={<FeedbackPage />} />
         <Route path="/secession-page" element={<SecessionPage />} />
-
         <Route path="/search-company" element={<SearchPage />} />
         <Route path="/search-result" element={<SearchResultPage />} />
         <Route path="/company-info/:companyId" element={<CompanyInfoPage />} />
