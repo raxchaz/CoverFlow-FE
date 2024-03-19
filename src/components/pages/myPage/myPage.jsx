a105d0d21e3de9a9237d34dae6bd7b6fed7e7f01
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-03-19 10:07:58 +0900

    refactor :  빌드 오류 수정

--- src/components/pages/myPage/myPage.jsx
+++ src/components/pages/myPage/myPage.jsx
@@ -10,10 +10,10 @@ import { ReactComponent as PremiumIcon } from '../../../asset/image/premium.svg'
 import { ReactComponent as NoticeIcon } from '../../../asset/image/notice.svg';
 
 import { StyledHeader, StyledPage } from '../../../styledComponent';
-import TitleHeader from '../../ui/header/titleHeader';
 
 import { ACCESS_TOKEN, REFRESH_TOKEN, BASE_URL } from '../../global/constants';
 import TabBar from '../../ui/tabBar/tabBar';
+import TitleHeader from '../../ui/header/titleHeader';
 
 /* 스타일 컴포넌트 정의 */
 const LogoutButton = styled.button`
