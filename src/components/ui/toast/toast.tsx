812217b8aabbed467adf85c8ae3c5ccb99065f13
JiwonShin <acd0825@naver.com>
2024-03-25 03:55:57 +0900

    (#213) style: 관리자 페이지 내 기업 관리 페이지 placeholder 수정합니다.

--- src/components/ui/toast/toast.tsx
+++ src/components/ui/toast/toast.tsx
@@ -1,12 +1,30 @@
-import { toast, ToastOptions } from 'react-toastify';
+import React from 'react';
+import { toast, ToastOptions, ToastContainer } from 'react-toastify';
+import errorIcon from '../../../asset/image/toast-error.svg';
+import successIcon from '../../../asset/image/toast-success.svg';
+import styled from 'styled-components';
+
+// ToastContainer를 커스텀 스타일링
+export const StyledToastContainer = styled(ToastContainer)`
+  margin-top: 60px;
+  .Toastify__toast {
+    background-color: black;
+    color: white;'
+    font: pretendard-regular;
+  }
+  .Toastify__close-button {
+    display: none;
+  }
+`;
 
 const baseToastOptions: ToastOptions = {
   position: 'top-center',
   autoClose: 2000,
-  hideProgressBar: false,
-  closeOnClick: true,
+  hideProgressBar: true,
+  closeOnClick: false,
   pauseOnHover: true,
   draggable: true,
+  closeButton: false,
   style: {
     textAlign: 'center',
   },
@@ -15,13 +33,25 @@ const baseToastOptions: ToastOptions = {
 export const showSuccessToast = (message: string) => {
   toast.success(message, {
     ...baseToastOptions,
-    style: { ...baseToastOptions.style, background: 'black', color: 'white' },
+    icon: <img src={successIcon} alt="success" />,
+    style: {
+      ...baseToastOptions.style,
+      background: 'black',
+      color: 'white',
+      font: 'pretendard-regular',
+    },
   });
 };
 
 export const showErrorToast = (message: string) => {
   toast.error(message, {
     ...baseToastOptions,
-    style: { ...baseToastOptions.style, background: 'black', color: 'white' },
+    icon: <img src={errorIcon} alt="error" />,
+    style: {
+      ...baseToastOptions.style,
+      background: 'black',
+      color: 'white',
+      font: 'pretendard-regular',
+    },
   });
 };
