048758b78752957ae5f4cbddd71beb1d1513aa1c
JiwonShin <acd0825@naver.com>
2024-04-08 02:39:51 +0900

    bugfix: 실시간 알림 수정합니다.

--- src/components/global/utils/eventApiUtils.js
+++ src/components/global/utils/eventApiUtils.js
@@ -19,28 +19,32 @@ export const initializeSSE = () => {
     heartbeatTimeout: 120000,
   });
 
-  const handleConnect = async () => {
-    const res = await fetch(`${BASE_URL}/api/notification/connect`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'text/event-stream; charset=utf-8',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    console.log('res', res);
-  };
   sse.addEventListener('connect', (event) => {
     const data = event;
     console.log('ssedata', data);
+    handleConnect();
   });
+
   sse.onmessage = (e) => {
     console.log('onmessage', e);
   };
+
   sse.onerror = (event) => {
     console.log('onerror', event);
     sse.close();
   };
 
-  handleConnect();
   return sse;
+};
+
+const handleConnect = async () => {
+  const accessToken = localStorage.getItem(ACCESS_TOKEN);
+  const res = await fetch(`${BASE_URL}/api/notification/connect`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'text/event-stream; charset=utf-8',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  console.log('res', res);
 };
--- src/components/ui/adminSelection/companyDetail.tsx
+++ src/components/ui/adminSelection/companyDetail.tsx
@@ -60,7 +60,6 @@ export default function CompanyDetail({
         type: companyType,
         city: companyCity,
         district: companyDistrict,
-
         companyStatus,
       });
       handleSearch();
