82005bc322fa82c4abc127a0a4a91884087b7a07
JiwonShin <acd0825@naver.com>
2024-05-31 02:08:51 +0900

    (#329)style: 버튼 공용 스타일 제거시 발생하는 css 오류 수정했습니다.

--- src/components/ui/calendar/calendar.tsx
+++ src/components/ui/calendar/calendar.tsx
@@ -46,6 +46,11 @@ const Box = styled.div`
 const DateContainer = styled(DatePicker)`
   width: 135px;
   height: 28px;
+
+  .react-datepicker__navigation{
+    width:auto !impotant;
+  }
+  .react-datepicker__navigation--next
 `;
 
 const Slash = styled.p`
